8ca637726bcc6454cb7decd96f11bbc7a9badd3d
Josh Gould <joshgould@Josh-Goulds-MacBook-Pro-3.local>
2014-10-14 18:50:31 -0700

    saving object arrays

--- cloud/main.js
+++ cloud/main.js
@@ -1,13 +1,34 @@
+Parse.Cloud.define("count", function(request, response) {
+  	var Message = Parse.Object.extend("Message");
+	var query = new Parse.Query(Message);
+	console.log("val " +request.params.fieldVal);
+	console.log("name " +request.params.fieldName);
+	
+	query.equalTo(request.params.fieldName,request.params.fieldVal);
+	query.count({
+	  success: function(count) {
+  		console.log("count of " +request.params.fieldVal + " for " + request.params.fieldName + " is: " + JSON.stringify(count));
+	 	response.success(count);
+	  },
+	  error: function(error) {
+	    // The request failed
+	  }
+	});
+});
+
+
 Parse.Cloud.define("read", function(request, response) {
-  	var query = new Parse.Query("Message");
-	query.select("content");
-	query.find({
-    success: function(results) {
+  	var Message = Parse.Object.extend("Message");
+	var query = new Parse.Query(Message);
+	query.select("idx","firstName","lastName","eyeColor");
+	query.find({    
+	success: function(results) {
 		response.success(results);
     },
     error: function() {
       response.error("message lookup failed");
     }
+	
   });
 });
 
@@ -16,20 +37,25 @@ Parse.Cloud.define("saveList", function(request, response) {
 
     for (var i=0; i<request.params.list.length; i++) {
         var Message = Parse.Object.extend("Message");
-        var message = new Message();
+        var message = new Message();		
         var content = request.params.list[i];
-		message.set("content","bar:" + content);   
-        messageList[i]=message; //add another item to list
+
+		console.log("content is: " + JSON.stringify(content));
+		message.set("firstName",content.firstName);   
+		message.set("lastName",content.lastName);   
+		message.set("eyeColor",content.eyeColor);   
+		message.set("idx",content.id);   
+		messageList[i]=message; //add another item to list
 	}
 
     Parse.Object.saveAll(messageList,{
     success: function(list) {
       // All the objects were saved.
-      response.success("ok " );  //saveAll is now finished and we can properly exit with confidence :-)
+      response.success("ok");  //saveAll is now finished and we can properly exit with confidence :-)
     },
     error: function(error) {
       // An error occurred while saving one of the objects.
-      response.error("failure on saving list ");
+      response.error("failure on saving list " + JSON.stringify(error));
     },
   });
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,41 +1,85 @@
 $(document).ready(function(){  
 	$content = $('#content');
 	$messages = $('#messages');
+	var fields = ['firstName','lastName','eyeColor'];
+	var firstName = ['Josh', 'Jan', 'Josie', 'Jacob'];
+	var lastName = ['Gould', 'Tai', 'Jones', 'Smith'];
+	var eyeColor = ['blue', 'brown', 'hazel', 'light brown'];
 	
-	function save(msg) {
-		var Message = Parse.Object.extend("Message");
-		var message = new Message();
-		message.save({content: msg});
+	function Person(id,firstName,lastName,eyeColor) {
+      this.id = id;	 
+	  this.firstName = firstName;
+	  this.lastName = lastName;
+	  this.eyeColor = eyeColor;
+  	}
+	
+	function random(list) {
+		var i = Math.floor(Math.random() * 4);
+		return (list[i]);
 	}
 	
 	function createArr(x) {
 		var arr = [];
-			for (var i=0; i<x; i++) {
-				arr.push(i);
-			} 		
+		for (var i=0; i<x; i++) {
+			var fname = random(firstName);
+			var lname = random(lastName);
+			var color = random(eyeColor);
+			var person = new Person(i,fname,lname,color);
+			arr.push(person);
+		}
 		return arr;
 	}
 		
-	function saveMsg() {
-		var arr = createArr(10);		
+	function saveMsg() {		
+		var arr = createArr(10);
+	//	console.log("arr is: " + JSON.stringify(arr));
+
 		var promise = Parse.Cloud.run('saveList', {list:arr}, {
 			success: function(msg) {
 				console.log("save msg sucess: " + msg);
-	     	},   
+	     	},
 			error: function(error) {
 				console.log("writeMsg error " + JSON.stringify(error));
 			}
 		});
-		return promise;	
+		return promise;
+	}	
+	
+	
+	
+	function displayCounts() {
+		console.log("Getting Counts");
+		$.each(fields, function(idx){
+			var fieldStr = fields[idx];
+			var fieldList = eval(fieldStr);
+
+			$.each(fieldList, function(index){
+			console.log("fieldName: " + fieldStr + ". fields: " + fieldList);
+			console.log("field at index: " + index + ": " + fieldList[index]);
+			Parse.Cloud.run('count', {fieldName:fieldStr, fieldVal:fieldList[index]}, {
+			 				success: function(count) {
+			 					$content.append("<p>" + fieldList[index] + " counts:" + count + "</p>");
+			 				},
+			 				error: function(error) {
+			 					console.log("displayAll error " + error);
+			 				}
+			 				});
+
+			});
+		});
+				
 	}	
 	
 	function displayMsg() {
 		console.log("Reading and displaying messages");
+		
 		Parse.Cloud.run('read', {}, {
 			success: function(msg) {
-				console.log("# messages:" + msg.length);
+				//console.log("# messages:" + msg.length);
+				//console.log("msg:" + JSON.stringify(msg));
+				
 				$.each(msg, function(index){
-		    		$messages.append("<p>" + msg[index].get("content") + "</p>"); 
+		    		$messages.append("<p>" + msg[index].get("idx")  + ": "+ msg[index].get("firstName") + " " + msg[index].get("lastName") + ",Eye Color: " + msg[index].get("eyeColor") + ".</p>"); 
 			});			
 			},
 			error: function(error) {
@@ -47,15 +91,16 @@ $(document).ready(function(){
 	
 
 	// Initialize Parse with your Parse application javascript keys
-	 Parse.initialize("idhQyKbKtbEjLeVwbBXiblxig5aPrLcxjVq5Z4mV",
+	Parse.initialize("idhQyKbKtbEjLeVwbBXiblxig5aPrLcxjVq5Z4mV",
                  "wV4RAehtUgibr876dY9dhsHRC6xQYnBLnqrUOVW3");
 
 	$content.empty();
 	$messages.empty();
 		
 	Parse.Promise.when(saveMsg()).done(function() {
-	    displayMsg();
-		console.log( 'success' );
+	    displayCounts();
+		displayMsg();
+		console.log('success');
 	});
 		
 });
